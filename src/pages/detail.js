842e02b7e836ee921d60a670cdacd4fa5616b1e9
ALEX B-DEV <alexbetbu@gmail.com.com>
2023-08-31 00:30:34 -0500

    Boton Navegaación

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -26,18 +26,23 @@ export default function Detail() {
     }, []);
 
     return(
-        <div>
-        {console.log(pokemonDetail)}
+        <div className="container">
+            <div className="card">
+            {console.log(pokemonDetail)}
 
-        <h1>Detalle de Pokemon</h1>
-        <h2>Nombre: {pokemonDetail.name}</h2>
-        <p>ID: {id}</p>
+            <h1>Detalle de Pokemon</h1>
+            <h2>Nombre: {pokemonDetail.name}</h2>
+            <p>ID: {id}</p>
 
-        {console.log(pokemonDetail.sprites)}
-        {console.log(pokemonDetail.types)}
-        {console.log(pokemonDetail.stats)}    
-        {console.log(pokemonDetail.abilities)}         
-        <br/> 
+            {console.log(pokemonDetail.sprites)}
+            {console.log(pokemonDetail.types)}
+            {console.log(pokemonDetail.stats)}    
+            {console.log(pokemonDetail.abilities)}         
+            <br/> 
+
+            <a className="btn btn-sm btn-dark" href={"/home"}> BACK POKEMON LIST </a>   
+
+        </div>
         </div>
     )
 }
